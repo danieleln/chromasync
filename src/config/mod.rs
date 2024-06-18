6ddf935b635220370e7830ed99c231aa90c05f7a
daniele <daniele.monzani@outlook.com>
2024-06-18 18:32:47 +0200

    refactor(config): split config.rs into submodules

---
+++ src/config/mod.rs
@@ -0,0 +1,13 @@
+#![allow(unused)]
+pub mod environ;
+
+/////////////////
+// Info Module //
+/////////////////
+pub mod info {
+    use const_format::formatcp;
+
+    pub const APP_NAME: &str = "chromasync";
+    pub const DESCRIPTION: &str = formatcp!("`{APP_NAME}` is a tool designed to automate the process of changing colorschemes for various terminal applications.
+");
+}
